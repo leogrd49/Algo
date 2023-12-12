d712219fb5d7785109efda342da5b16fb7bb2f81
leogrd49 <leogerard49.pro@gmail.com>
2023-12-12 14:14:34 +0100

    PATCH EXO2

--- Partie 5/exo2/python_exo2.py
+++ Partie 5/exo2/python_exo2.py
@@ -1,21 +1,25 @@
 i = 0
 word = ""
 taille_word = 0
+word_stock = []
 
-def count_word(word, taille_word):
+def count_word(word):
     taille_word = len(word)
     return taille_word
-
-
 
 print("Pour arrêter vos saisies entrez: STOP")
 
 while word != "STOP":
     word = input("Entrer vos mots: ")
-    i += count_word(word, taille_word)
+    
+    if word != "STOP":
+        taille = count_word(word)
+        word_stock.append(taille)
+        i += taille
 
-    if word == "STOP":
-        i -= 4
+    else:
+        i += 0  
 
 
-print(f"Le nombre total de lettres contenues dans chacun de vos mots est de: {i}")
+print(word_stock)
+print("Le total est de: ", i)
